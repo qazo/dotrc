9ec3c7d195c7bba2e2da498ba2119c293718d7bc
Kwezilomso Mhaga <kwezimhaga@live.com>
2021-09-25 15:18:27 +0200

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 
+set -e
 
-for file in zshrc zprofile tmux.conf; do
-	echo "${file}"
-	cp {,"${HOME}"/.}"${file}"
-done
+install_home_files() {
+	for file in zshrc zprofile tmux.conf; do
+		destfile="${HOME}/.${file}"
 
-echo "config"
-cp --recursive --no-target-directory {,"${HOME}"/.}config
+		if [ -f "${destfile}" ] || [ -L "${destfile}" ]; then
+			echo "${destfile} already exists. removing."
+			rm "${destfile}"
+		fi
+		echo "creating symlink for '${destfile}'"
+		ln -s "${PWD}/$file" "${destfile}"
+	done
+}
+
+install_bin_files() {
+	destdir="${HOME}/bin"
+	if [ ! -d "${destdir}" ]; then
+		mkdir -p "${destdir}"
+	fi
+
+	for file in ./bin/*; do
+		cp "${file}" "${destdir}"
+	done
+}
+
+install_config_dirs() {
+	cp --recursive --no-target-directory {,"${HOME}"/.}config
+}
+
+install_home_files;
+install_bin_files;
